952d0260b844cdd552ae4a51800129b6d165ff8a
igopib <gs109690@gmail.com>
2023-12-11 23:08:26 -0600

    feat: audit report final

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.18;
 /*
  * @author not-so-secure-dev
  * @title PasswordStore
- * @notice This contract allows you to store a private password that others won't be able to see. 
+ * @notice This contract allows you to store a private password that others won't be able to see.
  * You can update your password at any time.
  */
 contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+
+    // @audit private function will still provide data to non owners. s_password can be accessed by anyone
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +25,8 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
+    // @audit no only owner check on setPassword function. Can be executed by everyone.
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
